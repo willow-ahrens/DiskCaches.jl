79b6113bdb2623c056ca73199f76d216ed6ce921
Peter Ahrens <ptrahrens@gmail.com>
2021-01-28 20:45:45 -0500

    more docs!

--- src/DiskCaches.jl
+++ src/DiskCaches.jl
@@ -6,11 +6,11 @@ using Pidfile
 export DiskCache
 
 """
-  DiskCache(path)
+    DiskCache(path)
 
-  `DiskCache{K,V}(path)` constructs a cache with keys of type `K` and values of type `V`, mapped to the filesystem `path` using `serialize` and `deserialize`. `DiskCache` behaves like `Dict` by default, but `DiskCache{K,V,D}` behaves like `D`, an `AbstractDict{K, V}`.
+`DiskCache{K,V}(path)` constructs a cache with keys of type `K` and values of type `V`, mapped to the filesystem `path` using `serialize` and `deserialize`. `DiskCache` behaves like `Dict` by default, but `DiskCache{K,V,D}` behaves like `D`, an `AbstractDict{K, V}`.
 
-  A `DiskCache` supports fast reads of keys already on chip and slow writes of new keys to disk, but does not support modification of existing keys. While writes to existing keys will raise an error, it is always safe to call `get!`.
+A `DiskCache` supports fast reads of keys already on chip and slow writes of new keys to disk, but does not support modification of existing keys. While writes to existing keys will raise an error, it is always safe to call `get!`.
 """
 struct DiskCache{K,V,D<:AbstractDict{K, V}} <: AbstractDict{K,V}
     path::String
